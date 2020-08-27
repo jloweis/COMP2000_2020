f42452d93d0932fdf4b36a2b202ff933e00179df
jloweis <jaiden.oweis@students.mq.edu.au>
2020-08-27 20:08:50 +1000

    a

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import javax.swing.*;
 import java.awt.*;
 
@@ -19,22 +20,10 @@ class Main extends JFrame {
 
     }
 
+=======
+public class Main {
+>>>>>>> parent of 0240e12... tasks 3, 4, and 5 solutions
     public static void main(String[] args) throws Exception {
-        Main window = new Main();
-        window.run();
-    }
-
-    private Main() {
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        App canvas = new App();
-        this.setContentPane(canvas);
-        this.pack();
-        this.setVisible(true);
-    }
-
-    public void run() {
-        while (true) {
-            this.repaint();
-        }
+        System.out.println("Red vs. Blue");
     }
 }
